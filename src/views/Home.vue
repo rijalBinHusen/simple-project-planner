4b292b1dc9fd981370bac2acb4c1c04c75116d6a
rijal <jeryzal333@gmail.com>
2022-06-07 05:12:56 +0700

    Home, view, and data

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="home">
-    Home
+    <div v-if="projects.length">
+      <div v-for="project in projects" :key="project.id">
+        <p>{{ project.title }}</p>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -9,6 +13,17 @@
 
 export default {
   name: "Home",
+  data() {
+    return {
+      projects: []
+    }
+  },
   components: { },
+  mounted() {
+    fetch("http://localhost:3000/projects")
+      .then(res => res.json())
+      .then(data => this.projects = data)
+      .catch(err => console.log(err.message))
+  },
 };
 </script>
